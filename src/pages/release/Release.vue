614d8c2b302256dbb3655fce4791e757ee39cb34
Ch2x <1270427018@qq.com>
2018-03-07 00:05:47 +0800

    第三次提交

--- src/pages/release/Release.vue
+++ src/pages/release/Release.vue
@@ -14,16 +14,52 @@
                         <li class="pic_item" v-for="(item, index) in images" :key="index">
                             <img :src="item">
                             <a href="">
-                                <span>X</span>
+                                <span>x</span>
                             </a>
                         </li>
                         <li class="pic_item">
-                            <input type="file" accept="image/png" style="display: none;"/>
+                            <div class="pic_addItem">
+                                <span class="pic_add">
+                                <svg>
+                                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-add"></use>
+                                </svg>
+                                </span>
+                                <span class="pic_font">添加</span>
+                                <input type="file" accept="image/png" style="display: none;"/>
+                            </div>
                         </li>
                     </ul>
                 </section>
             </div>
+            <section>
+                <router-link to="/home" class="release_link">
+                    <div class="name">
+                        <p>分类</p>
+                    </div>
+                        <span>选择分类</span>
+                        <span class="arrow">
+                            <svg class="arrow-svg" fill="#bbb">
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
+                            </svg>
+                         </span>
+        
+                </router-link>
+                <div class="price_link">
+                    <div class="name">
+                        <p>价格</p>
+                    </div>
+                        <!-- <span>开个价</span> -->
+                        <input type="text">
+                        <!-- <span class="arrow">
+                            <svg class="arrow-svg" fill="#bbb">
+                                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
+                            </svg>
+                         </span> -->
+        
+                </div>
+            </section>
         </form>
+        <section class="releaseShop">确定发布</section>
     </div>
 </template>
 
@@ -33,13 +69,13 @@ import Header from "@/components/Header";
 export default {
   data() {
     return {
-        images: [
-            '../../assets/logo.png',
-            '../../assets/logo.png',
-            '../../assets/logo.png',
-            '../../assets/logo.png',
-            '../../assets/logo.png',
-        ],
+      images: [
+        "../../assets/logo.png",
+        "../../assets/logo.png",
+        "../../assets/logo.png",
+        "../../assets/logo.png",
+        "../../assets/logo.png"
+      ]
     };
   },
   components: {
@@ -60,50 +96,119 @@ export default {
 }
 .releaseForm {
   .container {
-      background-color: #fff;
-      padding: 0 0.8rem;
-      .input_container {
-        display: flex;
-        justify-content: space-between;
-        border-bottom: 1px solid #f1f1f1;
-        padding: .6rem 0;
-        input {
-            width: 90%;
-            @include sc(0.7rem, #666);
-        }
+    background-color: #fff;
+    padding: 0 0.8rem;
+    .input_container {
+      display: flex;
+      justify-content: space-between;
+      border-bottom: 1px solid #f1f1f1;
+      padding: 0.8rem 0;
+      input {
+        width: 90%;
+        @include sc(0.7rem, #666);
+      }
     }
     .textarea_container {
-        height: 5.2rem;
-        padding: .6rem 0;
-        textarea {
-            @include wh(100%, 100%);
-            @include sc(0.7rem, #666);
-        }
+      height: 6.6rem;
+      padding: 0.8rem 0;
+      textarea {
+        @include wh(100%, 100%);
+        @include sc(0.7rem, #666);
+      }
     }
     .picture_container {
-        .pic_list {
-            display: flex;
-            flex-wrap: wrap;
-            .pic_item {
-                position: relative;
-                width: 25%;
-                padding: .3rem;
-                @include fj(center);
-                img {
-                    margin-bottom: .3rem;
-                    @include wh(1.8rem, 1.8rem);
-                }
-                a {
-                    display: inline-block;
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    @include wh(.5rem, .5rem);
-                    border-radius: 50%;
-                }
-            }
+      .pic_list {
+        display: flex;
+        flex-wrap: wrap;
+        .pic_item {
+          position: relative;
+          width: 25%;
+          padding: 0.3rem;
+          @include fj(center);
+          img {
+            margin-bottom: 0.3rem;
+            @include wh(3.5rem, 3.5rem);
+          }
+          a {
+            display: inline-block;
+            position: absolute;
+            top: 0;
+            left: 0;
+            @include wh(0.5rem, 0.5rem);
+            border-radius: 50%;
+          }
         }
+      }
     }
+    .pic_addItem {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      @include wh(3.5rem, 3.5rem);
+      background-color: rgb(245, 245, 245);
+      .pic_add {
+        @include wh(1.2rem, 1.2rem);
+        display: inline-block;
+        svg {
+          @include wh(100%,100%);
+        }
+      }
+      .pic_font {
+        @include sc(0.7rem, #666);
+      }
+    }
+  }
+}
+.release_link {
+  display: flex;
+  align-items: center;
+  background: #fff;
+  margin-top: 0.5rem;
+  padding: 0.666667rem 0.6rem;
+  @include sc(0.7rem, #666);
+  .name {
+    margin-left: 0.48rem;
+    -webkit-box-flex: 1;
+    -ms-flex-positive: 1;
+    flex-grow: 1;
+  }
+  .arrow {
+    @include wh(0.46667rem,0.98rem);
+    display: inline-block;
+    svg {
+      @include wh(100%,100%);
+    }
+  }
+}
+.price_link {
+  display: flex;
+  align-items: center;
+  background: #fff;
+  padding: 0.666667rem 0.6rem;
+  @include sc(0.7rem, #666);
+  .name {
+    margin-left: 0.48rem;
+    -webkit-box-flex: 1;
+    -ms-flex-positive: 1;
+    flex-grow: 1;
+  }
+  input {
+    width: 20%;
+    border: 1px solid #666;
+  }
+}
+.releaseShop {
+  width: 96%;
+  margin: 1.3rem auto 0;
+  line-height: 1.5rem;
+  @include borderRadius(5px);
+  text-align: center;
+  background: #d8584a;
+  @include sc(0.6rem,#fff);
+  &:active {
+    opacity: 0.8;
+    background: #c1c1c1;
   }
 }
 </style>
--- src/pages/sort/Sort.vue
+++ src/pages/sort/Sort.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
+    <div class="sort_page">
         <Header head-title='分类'></Header>
+        <section>
+            <ul class="sortLists">
+                <li v-for="(item, index) in sortLists" :key="index">
+                    <router-link :to="{path: '/shopList', query: {name: item}}">
+                        <span>{{item}}</span>
+                    </router-link>
+                </li>
+            </ul>
+        </section>
         <Footer></Footer>
     </div>
 </template>
@@ -12,7 +21,9 @@ import Footer from "@/components/Footer";
 export default {
     data() {
         return {
-
+            sortLists: [
+                '手机数码','图书教材','家用电器','运动户外','美妆','衣服','游戏'
+            ]  
         }
     },
     components: {
@@ -23,5 +34,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+    .sort_page {
+        padding-top: 1.9rem;
+    }
+    .sortLists {
+        >li {
+            padding: .5rem .5rem;
+            background-color: #fff;
+            margin-bottom: .3rem;
+        }
+    }
 </style>
