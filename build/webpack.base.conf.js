05d9159ba5322237bfd97d0aa2bac26bfa7603ab
Ch2x <1270427018@qq.com>
2019-12-29 23:59:45 +0800

    add loader rem to px

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,7 +4,7 @@ const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
 
-function resolve (dir) {
+function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
 
@@ -27,9 +27,8 @@ module.exports = {
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+    publicPath: process.env.NODE_ENV === 'production' ?
+      config.build.assetsPublicPath : config.dev.assetsPublicPath
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
@@ -45,6 +44,10 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
+      },
+      {
+        test: /\.vue$/,
+        loader: resolve('loader/remTopx.js'),
       },
       {
         test: /\.js$/,
@@ -75,7 +78,7 @@ module.exports = {
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       },
-      { 
+      {
         test: /\.scss$/,
         loaders: ["style", "css", "sass"]
       }
--- loader/remTopx.js
+++ loader/remTopx.js
@@ -1,21 +1,19 @@
-const loaderUtils = require('loader-utils');
-
 const style = /<style lang="scss" scoped>([\s\S]+)<\/style>/gi
 
 module.exports = function (content) {
-    let _source = ''
-
-    if(style.test(content)) {
-        _source = content.match(style)[0]
-    }
-
-    let regExp = /(d+)rem/gi;
-
-    if(regExp.test(_source)) {
-        let res = _source.replace(regExp, function(value1, value2) {
-            return
-        })
-    } else {
-        return content
-    }
+  let _source = ''
+  
+  if (style.test(content)) {
+    _source = content.match(style)[0]
+  }
+  let regExp = /(\d*(\.\d+)?)rem/gi;
+  if (regExp.test(_source)) {
+    let res = _source.replace(regExp, function (value1, value2) {
+      console.log(value2)
+      return (Number(value2) * 20).toFixed() + 'px';
+    })
+    return content.replace(_source, res)
+  } else {
+    return content
+  }
 }
