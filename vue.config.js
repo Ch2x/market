e0c15ba4b5f4d0fc1cc192c73c86d4ec34ad44f9
Ch2x <1270427018@qq.com>
2020-03-20 00:16:00 +0800

    add 骨架屏

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
-
 const projectRoot = path.resolve(__dirname)
+const {
+  SkeletonPlugin
+} = require('page-skeleton-webpack-plugin')
 
 module.exports = {
   lintOnSave: true,
@@ -10,6 +12,22 @@ module.exports = {
       alias: {
         '@': `${projectRoot}/src`
       }
+    },
+    plugins: [
+      new SkeletonPlugin({
+        pathname: path.resolve(__dirname, `./shell`), // 用来存储 shell 文件的地址
+        staticDir: path.resolve(__dirname, './dist'), // 最好和 `output.path` 相同
+        routes: ['/'], // 将需要生成骨架屏的路由添加到数组中
+      })
+    ]
+  },
+
+  chainWebpack: (config) => { // 解决vue-cli3脚手架创建的项目压缩html 干掉<!-- shell -->导致骨架屏不生效
+    if (process.env.NODE_ENV !== 'development') {
+      config.plugin('html').tap(args => {
+        args[0].minify.removeComments = false
+        return args
+      })
     }
-  }
+  },
 }
